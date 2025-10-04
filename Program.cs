b73877dbc35495dd92ce78773b8bbbdc2f263b58
sergioAscurra-10 <sebastianperez.27102005@gmail.com>
2025-10-04 16:27:06 -0500

    .

--- Program.cs
+++ Program.cs
@@ -16,7 +16,12 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDistributedMemoryCache();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    options.InstanceName = "PortalAcademico_";
+});
+
 // 2. Configurar el servicio de Sesión para que use Redis.
 builder.Services.AddSession(options =>
 {
