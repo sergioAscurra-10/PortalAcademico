a55340a93f636318b0c281306271f2c42ba772bd
sergioAscurra-10 <sebastianperez.27102005@gmail.com>
2025-10-04 15:47:05 -0500

    nueva configuracion

--- obj/Debug/net9.0/PortalAcademico.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortalAcademico.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5313c3d02475db0ecf396c4a7a6f897c9fc0cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee0365af85e7aad7a1087fbf6f8a8040f9d965c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
