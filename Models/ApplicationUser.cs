7823c8e98f7191bba3250a61fd4e8f1a1186c0ba
sergioAscurra-10 <sebastianperez.27102005@gmail.com>
2025-10-03 23:40:48 -0500

    bdContext

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PortalAcademico.Models;
+
+namespace PortalAcademico.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> // <-- Cambiar aquí
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Matricula> Matriculas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Restricción: Un usuario no puede matricularse dos veces en el mismo curso.
+            builder.Entity<Matricula>()
+                .HasIndex(m => new { m.UsuarioId, m.CursoId })
+                .IsUnique();
+
+            // Restricción: HorarioInicio < HorarioFin
+            builder.Entity<Curso>()
+                .ToTable(b => b.HasCheckConstraint("CK_Curso_Horarios", "[HorarioFin] > [HorarioInicio]"));
+        }
+    }
+}
