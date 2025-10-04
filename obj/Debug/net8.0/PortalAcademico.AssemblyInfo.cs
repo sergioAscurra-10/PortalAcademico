955cd7f5a37a8a4e85d72f09ff77ff663407ac54
sergioAscurra-10 <sebastianperez.27102005@gmail.com>
2025-10-04 16:39:24 -0500

    nuevos

--- obj/Debug/net8.0/PortalAcademico.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortalAcademico.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159817e50b8ea5a23b3039cd0e53db161915d2df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a0493b30ee109eddd7468f94664ddb26fd522a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalAcademico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
